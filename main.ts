f04a88111fe98f90e1fd0bf373b2dbfed3b406e4
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-20 13:26:32 -0400

    ikikik

--- main.ts
+++ main.ts
@@ -5,4 +5,29 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variable for random numbers
+let num0: number
+let num1: number
+
+// cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.isGesture(Gesture.Shake.function() {
+  if(num0 > num1) {
+    num0 = randint(0, 99)
+    basic.showNumber(num0)
+    num1 = randint(0, 99)
+  }
+})
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showNumber(random_number0)
+})
+
+input.onButtonPressed(Button.A, function() {
+  basic.clearScreen()
+  let num1 = randint(0, 99)
+  basic.showNumber(random_number1)
+})
