78bf42f6e3add7418c53e3b1538a55ea46b45559
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-24 10:43:07 -0400

    fixed indentation

--- main.ts
+++ main.ts
@@ -16,27 +16,27 @@ let num1: number
 
 // if else statement for if num0 is bigger than num1
 input.onGesture(Gesture.Shake, function () {
-    if (num0 > num1) {
-        num0 = randint(0, 99)
-        basic.showNumber(num0)
-        num1 = randint(0, 99)
-        basic.showString('>')
-        basic.showNumber(num1)
-    } else {
-        basic.showNumber(num0)
-        basic.showString('<')
-        basic.showNumber(num1)
-    }
+  if (num0 > num1) {
+    num0 = randint(0, 99)
+    basic.showNumber(num0)
+    num1 = randint(0, 99)
+    basic.showString('>')
+    basic.showNumber(num1)
+  } else {
+    basic.showNumber(num0)
+    basic.showString('<')
+    basic.showNumber(num1)
+  }
 })
 
 // display number 1
 input.onButtonPressed(Button.A, function () {
-    basic.showString('#1')
-    basic.showNumber(num0)
+  basic.showString('#1')
+  basic.showNumber(num0)
 })
 
 // display number 2
 input.onButtonPressed(Button.B, function () {
-    basic.showString('#2')
-    basic.showNumber(num1)
+  basic.showString('#2')
+  basic.showNumber(num1)
 })
